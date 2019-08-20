a2017b76f78e715ed4de97421bb3253f0c9d8f98
Monicatffee <monica.echeverrt@gmail.com>
2019-08-19 22:18:44 -0500

    Instalacion fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { ImagenContactoComponent } from './imagen-contacto/imagen-contacto.component';
 import { NombreContactoComponent } from './nombre-contacto/nombre-contacto.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { BotonComponent } from './boton/boton.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderMenuContactosComponent } from './header-menu-contactos/header-menu-contactos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImagenContactoComponent,
     NombreContactoComponent,
-    ContactoComponent
+    ContactoComponent,
+    BotonComponent,
+    HeaderMenuContactosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
