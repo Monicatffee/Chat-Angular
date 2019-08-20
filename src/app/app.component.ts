cec8360c511dbcf695eeeb5c2bf8c8f788ede88e
Monicatffee <monica.echeverrt@gmail.com>
2019-08-19 21:38:03 -0500

    Modificacion contactos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat-frontend';
+  usuario = {
+    nombre: 'Gipsy',
+    url: 'https://www.ecured.cu/images/thumb/7/7b/Usuario.jpg/260px-Usuario.jpg'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImagenContactoComponent } from './imagen-contacto/imagen-contacto.component';
+import { NombreContactoComponent } from './nombre-contacto/nombre-contacto.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagenContactoComponent,
+    NombreContactoComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
