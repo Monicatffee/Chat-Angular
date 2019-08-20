40a3327ac5d8360a671350b40755452e9172b25d
Monicatffee <monica.echeverrt@gmail.com>
2019-08-19 22:19:26 -0500

    Componente boton

---
+++ src/app/boton/boton.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.css']
+})
+export class BotonComponent implements OnInit {
+  @Input() icono: {};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
